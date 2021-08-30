dfa60a37f667f8f022fb2212bc2efce6a3291ee4
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-08-30 14:32:44 +0530

    Push New Project

---
+++ XMLDemo/XMLDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+namespace XMLDemo
+{
+    //  This program demo how write XML file
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  To create file inside working directory
+            using (XmlWriter xmlWriter = XmlWriter.Create("test.xml"))
+            {
+                //  To Start Wrting Doc.
+                xmlWriter.WriteStartDocument();
+                //  To Write Root Element
+                xmlWriter.WriteStartElement("Students");
+
+                //  To Write Parent Element
+                xmlWriter.WriteStartElement("Student");
+                // To create attribute
+                xmlWriter.WriteAttributeString("RollNo", "100");
+                xmlWriter.WriteAttributeString("Name", "Scott");
+                xmlWriter.WriteString("scott@gmail.com");
+                //  To end Parent Element
+                xmlWriter.WriteEndElement();
+                //  To close Root Element
+                xmlWriter.WriteEndElement();
+
+                //  To Writing Doc.
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
+                Console.WriteLine("XML file written successfully");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XMLDemo/XMLDemo/ToReadXML.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+namespace XMLDemo
+{
+    class ToReadXML
+    {
+        static void Main()
+        {
+            using (XmlReader xmlReader = XmlReader.Create("test1.xml"))
+            {
+                xmlReader.ReadToFollowing("Student");
+                do
+                {
+                    xmlReader.ReadToFollowing("Name");
+                    Console.WriteLine("Name={0}", xmlReader.ReadElementContentAsString());
+
+                    xmlReader.ReadToFollowing("Location");
+                    Console.WriteLine("Location={0}", xmlReader.ReadElementContentAsString());
+
+                    Console.WriteLine("-----------------------------------------------");
+                } while (xmlReader.ReadToFollowing("Student"));
+            }
+            Console.ReadLine();
+        }
+    }
+}
